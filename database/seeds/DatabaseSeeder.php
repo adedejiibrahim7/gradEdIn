42c83f50ccadec18191e7556d9a9e4f5c5db5ae5
Adedeji Ibrahim <adedejiibrahim7@gmail.com>
2020-08-03 20:46:22 +0100

    seeder  fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,28 +12,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        DB::table('users')->insert([
-          0 =>  [
-//            'name' => "Admin",
-            'email' => "testa@mailnator.com",
-            'password' => bcrypt('password'),
-            'is_admin' => true,
-              'created_at' => date("Y-m-d H:i:s"),
-              'updated_at' => date("Y-m-d H:i:s")
-//            'phone'=>'+911234567890',
-
-        ],
-           1 => [
-//            'name' => "Admin",
-                'email' => "test2@mailnator.com",
-                'password' => bcrypt('password'),
-                'is_admin' => true,
-               'created_at' => date("Y-m-d H:i:s"),
-               'updated_at' => date("Y-m-d H:i:s")
-//            'phone'=>'+911234567890',
-
-            ]
-        ]);
+//        DB::table('users')->insert([
+//          0 =>  [
+////            'name' => "Admin",
+//            'email' => "testa@mailnator.com",
+//            'password' => bcrypt('password'),
+//            'is_admin' => true,
+//              'created_at' => date("Y-m-d H:i:s"),
+//              'updated_at' => date("Y-m-d H:i:s")
+////            'phone'=>'+911234567890',
+//
+//        ],
+//           1 => [
+////            'name' => "Admin",
+//                'email' => "test2@mailnator.com",
+//                'password' => bcrypt('password'),
+//                'is_admin' => true,
+//               'created_at' => date("Y-m-d H:i:s"),
+//               'updated_at' => date("Y-m-d H:i:s")
+////            'phone'=>'+911234567890',
+//
+//            ]
+//        ]);
 
         DB::table('skills')->insert([
            0 => [ 'skill' => 'Research'],
@@ -41,32 +41,32 @@ class DatabaseSeeder extends Seeder
            2 => [ 'skill' => 'Machine Learning (Python)']
          ]);
 
-        DB::table('profiles')->insert([
-            0 => [
-                'user_id' => 1,
-                'title' => 'Dr',
-                'avatar' => 'uploads/profile/image/sQE03pqv0ps8syfCmw6wPR2nZhN8sIwYQjApSfBY',
-                'first_name' => 'Helen',
-                'last_name' => 'Callagher',
-                'bio' => 'Lorem ipsum dolor',
-                'cv' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
-                'cover_letter' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")
-            ],
-            1 => [
-                'user_id' => 2,
-                'title' => 'Prof',
-                'avatar' => 'uploads/profile/image/sQE03pqv0ps8syfCmw6wPR2nZhN8sIwYQjApSfBY',
-                'first_name' => 'Adam',
-                'last_name' => 'Lallana',
-                'bio' => 'Lorem ipsum dolor',
-                'cv' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
-                'cover_letter' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")
-            ]
-        ]);
+//        DB::table('profiles')->insert([
+//            0 => [
+//                'user_id' => 1,
+//                'title' => 'Dr',
+//                'avatar' => 'uploads/profile/image/sQE03pqv0ps8syfCmw6wPR2nZhN8sIwYQjApSfBY',
+//                'first_name' => 'Helen',
+//                'last_name' => 'Callagher',
+//                'bio' => 'Lorem ipsum dolor',
+//                'cv' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
+//                'cover_letter' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
+//                'created_at' => date("Y-m-d H:i:s"),
+//                'updated_at' => date("Y-m-d H:i:s")
+//            ],
+//            1 => [
+//                'user_id' => 2,
+//                'title' => 'Prof',
+//                'avatar' => 'uploads/profile/image/sQE03pqv0ps8syfCmw6wPR2nZhN8sIwYQjApSfBY',
+//                'first_name' => 'Adam',
+//                'last_name' => 'Lallana',
+//                'bio' => 'Lorem ipsum dolor',
+//                'cv' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
+//                'cover_letter' => 'uploads/profile/docs/82nkD5BUJs73ptOxgjQ4PPyHYBnNG7OrOvi3f0SF.pdf',
+//                'created_at' => date("Y-m-d H:i:s"),
+//                'updated_at' => date("Y-m-d H:i:s")
+//            ]
+//        ]);
 
         DB::table('profile_skills')->insert([
             0 => [

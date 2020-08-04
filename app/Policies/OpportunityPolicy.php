2019c870e72425c5906f92b232688c301cf594f6
Adedeji Ibrahim <adedejiibrahim7@gmail.com>
2020-08-04 13:54:28 +0100

    policy fixing

--- app/Policies/OpportunityPolicy.php
+++ app/Policies/OpportunityPolicy.php
@@ -30,7 +30,7 @@ class OpportunityPolicy
      */
     public function view(User $user, opportunity $opportunity)
     {
-        return auth()->user()->id === $opportunity->user_id;
+        return auth()->user()->id == $opportunity->user_id;
 //        return $user->id == $opportunity->user_id;
     }
 

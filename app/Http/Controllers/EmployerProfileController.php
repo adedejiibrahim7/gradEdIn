8b6c6f9120a6905003c18744ecb4c877f6fc06fb
Adedeji Ibrahim <adedejiibrahim7@gmail.com>
2020-08-21 21:52:28 +0100

    oo

--- app/Http/Controllers/EmployerProfileController.php
+++ app/Http/Controllers/EmployerProfileController.php
@@ -29,8 +29,8 @@ class EmployerProfileController extends Controller
             'last_name' => $data['lastName'],
             'institution_name' => $data['institution'],
             'institution_website' => $data['institution'],
-            'phone' => $data['phone'],
-            'position' => $data['position']
+            'phone' => $data['phone']
+//            'position' => $data['position']
         ]);
 
         session()->flash('msg', 'Profile Created!');

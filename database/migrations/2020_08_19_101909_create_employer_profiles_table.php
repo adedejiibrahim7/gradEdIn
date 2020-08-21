11b84a9800630d5706a2d2d3b3378dc401262051
Adedeji Ibrahim <adedejiibrahim7@gmail.com>
2020-08-21 21:27:26 +0100

    kk

--- database/migrations/2020_08_19_101909_create_employer_profiles_table.php
+++ database/migrations/2020_08_19_101909_create_employer_profiles_table.php
@@ -22,7 +22,6 @@ class CreateEmployerProfilesTable extends Migration
             $table->string('last_name');
             $table->string('institution_name');
             $table->string('institution_website');
-//            $table->string('position');
             $table->string('phone')->nullable();
             $table->string('avatar')->nullable();
             $table->string('location')->nullable();
